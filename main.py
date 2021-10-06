14015bb7a73d1109cde56622dc6013c35c811417
Максим <maksgirya@mail.ru>
2021-10-06 12:51:21 +0300

    pre push commit 2

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def ErrorConnection(syndrome, HemmingDict):
+    if syndrome == '000':
+        print('Сообщение правильное')
+        return
+    incorrect_byte_num = int(syndrome[::-1], 2)
+    incorrect_byte = list(HemmigDict.keys())[incorrect_byte_num - 1]
+    print(f'Ошибка в {incorrect_byte}')
+    if HemmigDict[incorrect_byte] == 0:
+        HemmigDict[incorrect_byte] = 1
+    else:
+        HemmigDict[incorrect_byte] = 0
+    print('Исправленное сообщение: ', end='')
+    print(*list(HemmigDict.values()), end=' ')
+
+
+print('Введите сообщение:')
 message = input()
 
 HemmigDict = {'r1': '',
@@ -18,11 +34,4 @@ s3 = (HemmigDict['r3'] + HemmigDict['i2'] + HemmigDict['i3'] + HemmigDict['i4'])
 print('Синдром - ', (s1, s2, s3))
 
 syndrome = str(s1) + str(s2) + str(s3)
-incorrect_byte_num = int(syndrome[::-1], 2)
-incorrect_byte = list(HemmigDict.keys())[incorrect_byte_num - 1]
-print(f'Ошибка в {incorrect_byte}')
-if HemmigDict[incorrect_byte] == 0:
-    HemmigDict[incorrect_byte] = 1
-else:
-    HemmigDict[incorrect_byte] = 0
-print(*list(HemmigDict.values()), end=' ')
+ErrorConnection(syndrome, HemmigDict)
